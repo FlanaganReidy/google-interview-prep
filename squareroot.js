cdb3d3314596f68f7c4b367b4fda13d4c7c48dcc
Shannon <reidy.pf@gmail.com>
2018-01-27 13:40:16 -0600

    code finished, commeneted but no tests

--- squareroot.js
+++ squareroot.js
@@ -3,7 +3,18 @@
 
 
 //so we know that this is a function and that it takes in a single parameter,
-//that is the square whose radicand we want to find
+//that is the square whose squarroot we want to find
 function squareRootFn(square){
 
-}
+    //we're going to be guessing at what the squareroot is
+    let guess = square/3;
+
+    while(square/guess !== guess){
+      guess = ((square/guess)+ guess)/2
+      }
+    return guess;
+
+    }
+//not sure how to calculate the big O of this.
+//I think this O(n)
+console.log(squareRootFn(16));
