6aaa5846f8c331482d4f86239b19fcd10523919d
Shannon <reidy.pf@gmail.com>
2018-01-27 12:48:50 -0600

    initial commit for square root challenge

---
+++ squareroot.js
@@ -0,0 +1,9 @@
+// here we're going to try to write a function that, when given a number, will return
+//that number's square root
+
+
+//so we know that this is a function and that it takes in a single parameter,
+//that is the square whose radicand we want to find
+function squareRootFn(square){
+
+}
